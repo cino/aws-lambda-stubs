4c925e3686604f754b37aa994b33dce4463c6d4c
Ricardo Cino <ricardo@cino.io>
2025-10-21 18:18:01 +0000

    dynamodb

---
+++ src/dynamodb-stream.ts
@@ -0,0 +1,23 @@
+import type { DynamoDBRecord, DynamoDBStreamEvent } from 'aws-lambda';
+import deepmerge from 'deepmerge';
+import { DEFAULT_ACCOUNT_ID, DEFAULT_REGION } from './common';
+
+export const dynamoDBRecordStub = (overrides: Partial<DynamoDBRecord> = {}): DynamoDBRecord => {
+  return deepmerge(
+    {
+      awsRegion: DEFAULT_REGION,
+      eventID: '1',
+      eventName: 'INSERT',
+      eventSource: 'aws:dynamodb',
+      eventSourceARN: `arn:aws:dynamodb:${DEFAULT_REGION}:${DEFAULT_ACCOUNT_ID}:table/ExampleTable/stream/2020-04-01T00:00:00.000`,
+      eventVersion: '1.1',
+    },
+    overrides
+  );
+};
+
+export const dynamoDBStreamEventStub = (records: DynamoDBRecord[]): DynamoDBStreamEvent => {
+  return {
+    Records: records,
+  };
+};
---
+++ tests/dynamodb-stream.test.ts
@@ -0,0 +1,39 @@
+import { DEFAULT_ACCOUNT_ID, DEFAULT_REGION } from 'src/common';
+import { dynamoDBRecordStub, dynamoDBStreamEventStub } from 'src/dynamodb-stream';
+import { describe, expect, it } from 'vitest';
+
+describe('#dynamodb-stream', () => {
+  it('should return a valid event', () => {
+    const event = dynamoDBStreamEventStub([dynamoDBRecordStub()]);
+
+    expect(event).toEqual({
+      Records: [
+        {
+          awsRegion: 'us-east-1',
+          eventID: '1',
+          eventName: 'INSERT',
+          eventSource: 'aws:dynamodb',
+          eventSourceARN: `arn:aws:dynamodb:${DEFAULT_REGION}:${DEFAULT_ACCOUNT_ID}:table/ExampleTable/stream/2020-04-01T00:00:00.000`,
+          eventVersion: '1.1',
+        },
+      ],
+    });
+  });
+
+  it('should allow overrides', () => {
+    const event = dynamoDBStreamEventStub([dynamoDBRecordStub({ eventName: 'MODIFY' })]);
+
+    expect(event).toEqual({
+      Records: [
+        {
+          awsRegion: 'us-east-1',
+          eventID: '1',
+          eventName: 'MODIFY',
+          eventSource: 'aws:dynamodb',
+          eventSourceARN: `arn:aws:dynamodb:${DEFAULT_REGION}:${DEFAULT_ACCOUNT_ID}:table/ExampleTable/stream/2020-04-01T00:00:00.000`,
+          eventVersion: '1.1',
+        },
+      ],
+    });
+  });
+});
