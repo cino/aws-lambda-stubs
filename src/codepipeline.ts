c376a1dca81bed829ab57b2aa228f62ce6c4654f
Ricardo Cino <ricardo@cino.io>
2025-10-23 20:24:03 +0000

    codepipeline

---
+++ src/codepipeline.ts
@@ -0,0 +1,64 @@
+import type { CodePipelineEvent } from 'aws-lambda';
+import deepmerge from 'deepmerge';
+import type { Merge } from 'type-fest';
+import { DEFAULT_ACCOUNT_ID } from './common';
+
+type PartialCodePipelineEvent = {
+  'CodePipeline.job'?: Merge<
+    Partial<CodePipelineEvent['CodePipeline.job']>,
+    {
+      data: Partial<CodePipelineEvent['CodePipeline.job']['data']>;
+    }
+  >;
+};
+
+export const codePipelineEventStub = (overrides: PartialCodePipelineEvent = {}): CodePipelineEvent => {
+  return deepmerge(
+    {
+      'CodePipeline.job': {
+        id: 'unique-job-id',
+        accountId: DEFAULT_ACCOUNT_ID,
+        data: {
+          actionConfiguration: {
+            configuration: {
+              FunctionName: 'my-lambda-function',
+              UserParameters: 'some-user-parameters',
+            },
+          },
+          inputArtifacts: [
+            {
+              name: 'InputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'input-artifact-bucket',
+                  objectKey: 'input/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          outputArtifacts: [
+            {
+              name: 'OutputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'output-artifact-bucket',
+                  objectKey: 'output/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          artifactCredentials: {
+            accessKeyId: 'artifact-access-key-id',
+            secretAccessKey: 'artifact-secret-access-key',
+            sessionToken: 'artifact-session-token',
+          },
+        },
+      },
+    },
+    overrides
+  ) as CodePipelineEvent;
+};
---
+++ tests/codepipeline.test.ts
@@ -0,0 +1,116 @@
+import { codePipelineEventStub } from 'src/codepipeline';
+import { DEFAULT_ACCOUNT_ID } from 'src/common';
+import { describe, expect, it } from 'vitest';
+
+describe('#codepipeline', () => {
+  it('should return a valid event', () => {
+    const event = codePipelineEventStub();
+
+    expect(event).toEqual({
+      'CodePipeline.job': {
+        id: 'unique-job-id',
+        accountId: DEFAULT_ACCOUNT_ID,
+        data: {
+          actionConfiguration: {
+            configuration: {
+              FunctionName: 'my-lambda-function',
+              UserParameters: 'some-user-parameters',
+            },
+          },
+          inputArtifacts: [
+            {
+              name: 'InputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'input-artifact-bucket',
+                  objectKey: 'input/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          outputArtifacts: [
+            {
+              name: 'OutputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'output-artifact-bucket',
+                  objectKey: 'output/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          artifactCredentials: {
+            accessKeyId: 'artifact-access-key-id',
+            secretAccessKey: 'artifact-secret-access-key',
+            sessionToken: 'artifact-session-token',
+          },
+        },
+      },
+    });
+  });
+
+  it('should allow overrides', () => {
+    const event = codePipelineEventStub({
+      'CodePipeline.job': {
+        data: {
+          actionConfiguration: {
+            configuration: {
+              FunctionName: 'overridden-lambda-function',
+              UserParameters: 'overridden-user-parameters',
+            },
+          },
+        },
+      },
+    });
+
+    expect(event).toEqual({
+      'CodePipeline.job': {
+        id: 'unique-job-id',
+        accountId: DEFAULT_ACCOUNT_ID,
+        data: {
+          actionConfiguration: {
+            configuration: {
+              FunctionName: 'overridden-lambda-function',
+              UserParameters: 'overridden-user-parameters',
+            },
+          },
+          inputArtifacts: [
+            {
+              name: 'InputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'input-artifact-bucket',
+                  objectKey: 'input/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          outputArtifacts: [
+            {
+              name: 'OutputArtifactName',
+              revision: 'artifact-revision-id',
+              location: {
+                type: 'S3',
+                s3Location: {
+                  bucketName: 'output-artifact-bucket',
+                  objectKey: 'output/artifact/key.zip',
+                },
+              },
+            },
+          ],
+          artifactCredentials: {
+            accessKeyId: 'artifact-access-key-id',
+            secretAccessKey: 'artifact-secret-access-key',
+            sessionToken: 'artifact-session-token',
+          },
+        },
+      },
+    });
+  });
+});
